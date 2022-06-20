4be17bf10591f917ef846c44f4722ee2ae09e759
CHETHAN R <91748752+chethan7760@users.noreply.github.com>
2022-06-20 16:57:34 +0530

    First commit

---
+++ ProductModule/src/main/java/c2tc/batch/product/ProductService.java
@@ -0,0 +1,38 @@
+package c2tc.batch.product;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+@Transactional
+public class ProductService {
+	@Autowired
+	private ProductRepository repo;
+	
+	//to retrive a data 
+	public List<Product> listAll() {
+		
+		return repo.findAll();
+	}
+	
+	//to retrieve a sepcific row
+	public Product get(Integer id) {
+		
+		return repo.findById(id).get();
+	}
+	
+	//create
+	public void save(Product product) {
+		 repo.save(product);		
+	}
+
+	//delete
+	public void delete(Integer id) {
+		repo.deleteById(id);
+		
+	}
+
+}
